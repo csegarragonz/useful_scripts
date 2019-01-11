fa1322afc78e81b49d6a621ff624e8adb0c446fa
SEGARRA GONZÁLEZ Carlos <cse@csem.ch>
2019-01-11 09:51:06 +0100

    Modified the tex compilation script to look for locally defined compilation scripts before using mine. This becomes specially necessary in collaboration projects.

--- compile_tex.sh
+++ compile_tex.sh
@@ -1,20 +1,25 @@
 #!/bin/bash
 
-# First look for the main tex file if there are various tex files
-filename="${1%%.*}"
-shopt -s nullglob
-for i in *.latexmain; do
-    filename="${i%%.*}"
-done
-filetexname="$filename.tex"
-echo $filetexname
-
-# Compile it with references if a .bib document exists in the current directory
-pdflatex $filetexname
-count=`ls -1 *.bib 2>/dev/null | wc -l`
-if [ $count != 0 ]
+# If there is a local compilation file that overrides ours we will use that.
+# Note that the list of names is completely arbitrary and might grow.
+if [ -f make.sh ];
 then
-bibtex $filename
+    ./make.sh
+else
+    # We then look for the main tex file if there are various tex files
+    filename="${1%%.*}"
+    shopt -s nullglob
+    for i in *.latexmain; do
+        filename="${i%%.*}"
+    done
+    filetexname="$filename.tex"
+    # Compile it with references if a .bib document exists
+    count=`ls -1 *.bib 2>/dev/null | wc -l`
+    pdflatex $filetexname
+    if [ $count != 0 ];
+    then
+    bibtex $filename
+    fi
+    pdflatex $filetexname
+    pdflatex $filetexname
 fi
-pdflatex $filetexname
-pdflatex $filetexname
